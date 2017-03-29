50e2c918f7feb118d4108c4e8cdb71b0ee22e8c2
oliver-jk-redding <oliver.jk.redding@gmail.com>
2017-03-29 10:35:09 +0100

    my changes

--- 1_Particles/js/SimpleParticle.js
+++ 1_Particles/js/SimpleParticle.js
@@ -1,74 +1,74 @@
 function Particle(posx, posy) {
 
 	// the position of the particle
-	this.posX = posx; 
-	this.posY = posy; 
-	// the velocity 
-	this.velX = 0; 
-	this.velY = 0; 
-	
+	this.posX = posx;
+	this.posY = posy;
+	// the velocity
+	this.velX = 0;
+	this.velY = 0;
+
 	// multiply the particle size by this every frame
-	this.shrink = 1; 
-	this.size = 1; 
-	
+	this.shrink = 1;
+	this.size = 1;
+
 	// multiply the velocity by this every frame to create
-	// drag. A number between 0 and 1, closer to one is 
+	// drag. A number between 0 and 1, closer to one is
 	// more slippery, closer to 0 is more sticky. values
-	// below 0.6 are pretty much stuck :) 
-	this.drag = 1; 
-	
+	// below 0.6 are pretty much stuck :)
+	this.drag = 1;
+
 	// add this to the yVel every frame to simulate gravity
-	this.gravity = 0; 
-	
+	this.gravity = 0;
+
 	// current transparency of the image
-	this.alpha = 1; 
+	this.alpha = 1;
 	// subtracted from the alpha every frame to make it fade out
-	this.fade = 0; 
+	this.fade = 0;
 
 	this.colour = "white";
-	
+
 
 	this.update = function() {
-	
+
 		// simulate drag
-		this.velX *= this.drag; 
+		this.velX *= this.drag;
 		this.velY *= this.drag;
-		
-		// add gravity force to the y velocity 
-		this.velY += this.gravity; 
-		
+
+		// add gravity force to the y velocity
+		this.velY += this.gravity;
+
 		// and the velocity to the position
 		this.posX += this.velX;
-		this.posY += this.velY; 
-		
+		this.posY += this.velY;
+
 		// shrink the particle
 		this.size *= this.shrink;
-		
+
 		// and fade it out
-		this.alpha -= this.fade; 
-	 
+		this.alpha -= this.fade;
+
 	};
-	
+
 	this.render = function(c) {
-		
+
 		c.save(); // save the current draw state
-		
-		if(this.alpha<0.01) return; 
-		
+
+		if(this.alpha<0.01) return;
+
 		// set the fill style
-		c.fillStyle = this.colour; 
-		
+		c.fillStyle = this.colour;
+
 		// and alpha
-		c.globalAlpha = this.alpha; 
-		
+		c.globalAlpha = this.alpha;
+
 		// draw a circle of the required size
 		c.beginPath();
 		c.arc(this.posX, this.posY, this.size, 0, Math.PI*2, true);
-	
+
 		// and fill it
 		c.fill();
-		
-		c.restore(); 
+
+		c.restore();
 	};
 
 
